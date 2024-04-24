0759b1185955484d5b2181800b2222418cd128e9
zakaria <nokra1717@gmail.com>
2024-04-24 13:20:29 +0100

    Initialisation du projet

---
+++ src/main/java/com/pfa/digitalisation/DigitalisationApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.digitalisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfa/digitalisation/DigitalisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.digitalisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
